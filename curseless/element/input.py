fa50ce01fffe66e85d5a3c745bb16615fda153b6
Billy Kaplan <billy1kaplan@gmail.com>
2019-03-31 13:37:08 -0400

    Lint elements

--- curseless/element/input.py
+++ curseless/element/input.py
@@ -2,17 +2,31 @@ class InputNode:
     """
     A node for text input.
     """
-    noop_key = lambda x, y, z: None
+    def noop_key(key, text):
+        return None
 
-    def __init__(self, location, echo=True, on_key=noop_key):
+    def noop_id(key, text):
+        return text
+
+    def __init__(self,
+                 location,
+                 echo=True,
+                 on_key=noop_key,
+                 transformer=noop_id):
         self.location = location
         self.echo = echo
         self.on_key = on_key
+        self.transformer = transformer
 
     def render(self, offset, renderer, observer):
         def wrap(*args, **kwargs):
             observer.notify(self.on_key(*args, **kwargs))
-        renderer.add_input(self, self.location.plus(offset), wrap, self.echo)
+        renderer.add_input(self.location.plus(offset), '',
+                           on_key=wrap,
+                           echo=self.echo,
+                           transformer=self.transformer)
+        renderer.focus_update(self.location.plus(offset))
 
     def clear(self, offset, renderer, observer):
-        renderer.update(offset.plus(self.location), ' ' * 100, '')
+        renderer.update(offset.plus(self.location), ' ' * 100, '', None)
+        renderer.clear_input(self)
--- curseless/element/textnode.py
+++ curseless/element/textnode.py
@@ -2,13 +2,13 @@ class TextNode:
     """
     Represents some text
     """
-    def __init__(self, location, text, style = None):
+    def __init__(self, location, text, style=None):
         self.location = location
         self.text = text
         self.style = style
 
     def render(self, offset, renderer, observer):
-        renderer.text(self, self.location.plus(offset), self.text, style = self.style)
+        renderer.text(self.location.plus(offset), self.text, self.style)
 
     def clear(self, offset, renderer, observer):
-        renderer.update(self.location.plus(offset), self.text, '')
+        renderer.update(self.location.plus(offset), self.text, '', self.style)
